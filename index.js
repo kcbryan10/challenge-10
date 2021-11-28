07f8c409e44bb5f6fd3cb864463847fa07bfb3d1
kcbryan <kcbryan10@gmail.com>
2021-11-28 17:31:22 -0600

    add employee function

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const addManager = () => {
         },
         {
             type: "input",
-            name: "nofficeNumber",
+            name: "officeNumber",
             message: "please enter a office number for the manager!",
             validate: inputNumber => {
                 if (inputNumber){
@@ -61,3 +61,80 @@ const addManager = () => {
         }
     ])
 }
+
+const addEmployee= () => {
+    return inquirer.prompt ([
+        {
+            type: "list",
+            name: "role",
+            message: "please choose a role for your employee",
+            choices: ["Intern", "Engineer"]
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "whats the name of the employee?",
+            validate: inpuName => {
+                if (inputName){
+                    return true
+                } else {
+                    console.log("please enter a name for the employee!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "please enter the employees id number",
+            validate: inputID=> {
+                if (inputID){
+                    return true
+                } else {
+                    console.log("please enter a id for the eployee!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "please enter an email for the employee!",
+            validate: inputEmail => {
+                if (inputEmail){
+                    return true
+                } else {
+                    console.log("please enter a email for the employee!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "please enter a github username for the employee!",
+            validate: inputNumber => {
+                if (inputNumber){
+                    return true
+                } else {
+                    console.log("please enter a github username for the employee!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "whats the name of the employee?",
+            validate: inpuName => {
+                if (inputName){
+                    return true
+                } else {
+                    console.log("please enter a name for the employee!");
+                    return false;
+                }
+            }
+        },
+    ])
+}
+
